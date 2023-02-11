fd38d616f974006bb66b273c77cb4af833bb3110
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 14:20:33 -0500

    Added shinyStorePlus

--- AddImageVideoToShinyApp/server.R
+++ AddImageVideoToShinyApp/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyStorePlus)
 # use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
 # options(shiny.maxRequestSize = 9*1024^2)
 
