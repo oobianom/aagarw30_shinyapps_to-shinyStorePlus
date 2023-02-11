bd0cb418559b1dd459c67a42cbe32d99c42720a3
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 14:45:50 -0500

    Added shinyStorePlus

--- TextInputWidgetExample/server.R
+++ TextInputWidgetExample/server.R
@@ -1,18 +1,20 @@
 library(shiny)
 
 shinyServer(
-  
-  function(input, output) {
-    
+
+  function(input, output, session) {
+
     output$myname <- renderText({
      # paste("My Name is :", input$name)
       input$name
       })
-    
+
     output$myage <- renderText({
       # paste("My Age is : ", input$age)
       input$age
     })
-    
+    # Include at the bottom
+    appid = "sample6"
+    setupStorage(appId = appid, inputs = TRUE)
   }
 )
