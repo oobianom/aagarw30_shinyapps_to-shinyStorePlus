946e5c1394064ec6801ad23f68900c25e2306b4d
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 14:01:19 -0500

    Update server.R

--- AddImageVideoToShinyApp/server.R
+++ AddImageVideoToShinyApp/server.R
@@ -2,7 +2,7 @@ library(shiny)
 # use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
 # options(shiny.maxRequestSize = 9*1024^2)
 
-shinyServer(function(input,output){
+shinyServer(function(input,output,session){
   
   # This reactive function will take the inputs from UI.R and use them for read.table() to read the data from the file. It returns the dataset in the form of a dataframe.
   # file$datapath -> gives the path of the file
@@ -40,4 +40,10 @@ shinyServer(function(input,output){
     else
       tabsetPanel(tabPanel("About file", tableOutput("filedf")),tabPanel("Data", tableOutput("table")),tabPanel("Summary", tableOutput("sum")))
   })
+  
+  
+  # Include shinyStorePlus declarations
+  appid = "application501"
+  setupStorage(appId = appid,inputs = TRUE)
+
 })
