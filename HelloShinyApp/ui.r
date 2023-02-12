45c0ca113c596aae687c902ad2ec037cf905cac2
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 20:58:19 -0500

    Added shinyStorePlus

--- HelloShinyApp/ui.r
+++ HelloShinyApp/ui.r
@@ -8,10 +8,11 @@ shinyUI(fluidPage(
 # fluid pages scale their components in realtime to fill all available browser width.
   # Header Panel : Create a header panel containing an application title.
   titlePanel(title = "Hello Shiny, this is title panel"),
-  # SidebarLayout():It creates a layout with a sidebar and main area. 
+  # SidebarLayout():It creates a layout with a sidebar and main area.
   sidebarLayout(
     # Sidebar panel
-  sidebarPanel(h2("this is the sidebarpanel for input widgets 2"), 
+    textInput("sample","Sample text","www.rpkg.net"),
+  sidebarPanel(h2("this is the sidebarpanel for input widgets 2"),
                h3("this is the sidebarpanel for input widgets 3"),
                h4("this is the sidebarpanel for input widgets 4"),
                h5("this is the sidebarpanel for input widgets 5")
@@ -21,8 +22,8 @@ shinyUI(fluidPage(
             h3("this is the main panel for display of output"),
             h4("this is the main panel for display of output"),
             h5("this is the main panel for display of output")
-            
-            
+
+
             )
   )
   )
