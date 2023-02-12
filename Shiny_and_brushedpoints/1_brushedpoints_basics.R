f82c60dd7bf4886ff782407a925c1cf3d3d46c58
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 21:46:25 -0500

    shinyStorePlus Not working

--- Shiny_and_brushedpoints/1_brushedpoints_basics.R
+++ Shiny_and_brushedpoints/1_brushedpoints_basics.R
@@ -7,11 +7,12 @@
 ## install the packages if not already before loading them
 library(shiny)
 library(ggplot2) # used to plot ggplot2 graphs
-
-## ui code starts here ## 
+library(shinyStorePlus)
+## ui code starts here ##
 ui <- fluidPage(
+  initStore(),
   h4("Demo - brushedPoints - Interactive plots - select data points in plot - return the rows of data that are selected by brush"),
-  
+
 # brush argument will enable the brush, sends the data point information to the server side
 # at the server side the data points under the brush related information can be read through input$BRUSHID
   plotOutput(outputId = "graph", brush = "plot_brush"), # brush ID is plot_brush
@@ -21,45 +22,49 @@ ui <- fluidPage(
     # defined the width of each column and also some styling (bold & italics) using tags
     column(width = 5, tags$b(tags$i("Actual Dataset")), tableOutput("data")),
     column(width=5, tags$b(tags$i("Rows corresponding to datapoints under brush")),  tableOutput("data_brush"), offset = 2)
-    
+
   )
-  
-  
+
+
 )
 
 
 ### server side code begins here ###
 server <- function(input, output, session) {
 
-  
+
   # scatter plot the mtcars dataset - mpg vs hp
   output$graph <- renderPlot({
     ggplot(data = mtcars, aes(x = mpg, y = hp)) +
       geom_point()
   })
-  
-    
+
+
   # To display the mtcars dataset on the left side in the app
   output$data <- renderTable({
     mtcars
   })
-  
-  
+
+
   # brushedPoints returns the row of data under the brush
   # brush information comes from ui to server using brushId and can be accessed using input$brushId
 
  output$data_brush <-  renderTable({
    n = nrow(brushedPoints(mtcars, brush = input$plot_brush)) # row count will be 0 when no selection made by the brush
-   if(n==0)  
+   if(n==0)
      return()
    else
      brushedPoints(mtcars, brush = input$plot_brush) # return rows
    # argument allRows = TRUE can also be used
-   ## It will add another column (selected_) to the actual dataset. True indicates that data point 
+   ## It will add another column (selected_) to the actual dataset. True indicates that data point
    # corresponding to that row was under the brush. False means data corresponding to that row wasn't selected by brush
 
-    
+
   })
+
+ # Include at the bottom
+ appid = "appl656"
+ setupStorage(appId = appid, inputs = TRUE)
 }
 
 shinyApp(ui, server)
--- Shiny_and_brushedpoints/2_brushed_points_remove_datapoints.R
+++ Shiny_and_brushedpoints/2_brushed_points_remove_datapoints.R
@@ -7,16 +7,17 @@
 ## install the packages if not already before loading them
 library(shiny)
 library(ggplot2) # used to plot ggplot2 graphs
-
+library(shinyStorePlus)
 # UI Code begins here
- 
+
 ui <- fluidPage(
+  initStore(),
   h4("Demo - brushedPoints - Interactive plots - select data points in plot - return the rows of data that are selected by brush"),
-  
+
   # brush argument will enable the brush, sends the data point information to the server side
   # at the server side the data points under the brush related information can be read through input$BRUSHID
   plotOutput(outputId = "boxplot", brush = "plot_brush_"), # brush ID is plot_brush, brush argument enables the brush
-  
+
   fixedRow(
     # left side actual dataset and right side the rows for datapoints selected by brush
     # defined the width of each column and also some styling (bold & italics) using tags
@@ -24,48 +25,51 @@ ui <- fluidPage(
     column(width= 5, tags$b(tags$i("Actual Dataset")),  tableOutput("data1")),
     column(width = 5, tags$b(tags$i("Updated Dataset")), tableOutput("data2"), offset = 2)
   )
-  
-  
-  
+
+
+
 )
 
 # Server code begins here
-server <- function(input, output) {
-  
+server <- function(input, output,session) {
+
   ## using the mtcars dataset
   mtcars1 = mtcars
   mtcars1$cyl = as.factor(mtcars1$cyl)
   mt <- reactiveValues(data=mtcars1) # making the dataset reactiveValues so that any changes in mt$data later could be reflected throughout
 
-  
+
   # Create boxplot object plotOutput function is expecting
   output$boxplot <- renderPlot({
     ggplot(mt$data, aes(cyl, mpg)) + geom_boxplot(outlier.colour = "red") + coord_flip()
     # outlier.colour = "red" makes the outlier points red in color
   })
-  
+
   ## Returns the actual dataset
   output$data1 <- renderTable({
     mtcars1
   })
-  
+
   ## Returns the updated state of the dataset
   output$data2 <- renderTable({
     mt$data # using the reactive object
   })
 
- 
+
   # Observe function
   # listening to brushing event
   # input$plot_brush_ fetches the information (rows) corresponding to data points under the brush
   # using the reactive object mt$data so that any changes done on it reflects to other reactive functions where it is used
     observe({
-    df = brushedPoints(mt$data, brush = input$plot_brush_, allRows = TRUE) 
+    df = brushedPoints(mt$data, brush = input$plot_brush_, allRows = TRUE)
     mt$data = df[df$selected_== FALSE,  ] ## Taking only those data points where the selected_ value is FALSE (alternatively ignoring rows with selected_ = TRUE status)
 
-    
+
   })
-  
+
+    # Include at the bottom
+    appid = "appl656"
+    setupStorage(appId = appid, inputs = TRUE)
 }
 
 # Create a Shiny app object
