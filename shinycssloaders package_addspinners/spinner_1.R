d6e166dc73edf46ac2bdb7acb46d78b51bbcfa7c
Obi Obianom <oobianom@umaryland.edu>
2023-02-12 00:04:28 -0500

    Added shinyStorePlus

--- shinycssloaders package_addspinners/spinner_1.R
+++ shinycssloaders package_addspinners/spinner_1.R
@@ -5,33 +5,37 @@
 ## Load required packages
 library(shiny) # for shiny components
 library(plotly) # for plotly chart
+library(shinyStorePlus)
 ## install.packages("shinycssloaders")  ## install the package if not already
 library(shinycssloaders)  # for animated CSS loader
 
 
 ## ui component begins here
 ui <- fluidPage(
+  initStore(),
   h4("Introduction to shinycssloaders package (A package by Andras Sali)"),
   h5("A demo example of adding an animated CSS spinner/loader while R plot is loading"),
     # with the spinner
   withSpinner(plotlyOutput("plot1"))
-) 
+)
 
 ## server component begins here
 server <- function(input, output, session) {
 
- ## Rendering a plotly graph 
+ ## Rendering a plotly graph
 output$plot1 <- renderPlotly({
     Sys.sleep(5) # Adding intentional delay in the execution of plot to demo the loading spinner.
-    plot_ly(data=mtcars, 
-            x=~wt, 
+    plot_ly(data=mtcars,
+            x=~wt,
             y=~mpg,
-            type = "scatter", 
+            type = "scatter",
             mode = "markers",
             color = ~as.factor(cyl),
             colors = "Set1")
   })
-  
+# Include at the bottom
+appid = "apprrlssat54on501"
+setupStorage(appId = appid, inputs = TRUE)
 }
 
 shinyApp(ui, server)
--- shinycssloaders package_addspinners/spinner_2.R
+++ shinycssloaders package_addspinners/spinner_2.R
@@ -3,29 +3,33 @@
 ## Load the required packages
 library(shiny) # for shiny components
 library(ggplot2) # for plotly chart
+library(shinyStorePlus)
 library(shinycssloaders)  # for animated CSS loader
 
 ## ui component begins here
 ui <- fluidPage(
+  initStore(),
   h5("A demo example of adding an animated CSS spinner/loader while R plot is loading"),
   sliderInput(inputId = "bw", label = "Enter the binwidth", min=50, max=500 , value=100 , step=50),
   withSpinner(plotOutput("plot1")) # withSpinner is used to add spinner
-) 
+)
 
 ## server component begins here
 server <- function(input, output, session) {
-  
-  ## Rendering a ggplot histogram from diamonds dataset 
+
+  ## Rendering a ggplot histogram from diamonds dataset
   output$plot1 <- renderPlot({
-    Sys.sleep(2) # adding intentional delay of 2 seconds for demo sake. 
-    ggplot(diamonds) + 
-      geom_histogram(binwidth = input$bw, aes(x=diamonds$price)) + 
-      ggtitle("Diamond Price Distribution") + 
-      xlab(paste("Diamond Price U$ - Binwidth", input$bw, sep = "")) + 
-      ylab("Frequency") + 
+    Sys.sleep(2) # adding intentional delay of 2 seconds for demo sake.
+    ggplot(diamonds) +
+      geom_histogram(binwidth = input$bw, aes(x=diamonds$price)) +
+      ggtitle("Diamond Price Distribution") +
+      xlab(paste("Diamond Price U$ - Binwidth", input$bw, sep = "")) +
+      ylab("Frequency") +
       theme_minimal()
   })
-  
+  # Include at the bottom
+  appid = "a33pplssat54on"
+  setupStorage(appId = appid, inputs = TRUE)
 }
 
 shinyApp(ui, server)
