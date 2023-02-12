5bf9b7580fe5a223a7a5fa575e66ef774b57da20
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 19:19:12 -0500

    Added shinyStorePlus

--- dir/server.R
+++ dir/server.R
@@ -5,15 +5,18 @@ library(maptools)
 india <- readShapePoly("india_st")
 literacy <- read.csv("literacy.csv")
 
-shinyServer(function(output,input)({
+shinyServer(function(output,input,session)({
   output$ind <- renderPlot({
-    
+
     rrt <- literacy[,as.numeric(input$year)]
     brks <- quantile(rrt, seq(0,1,1/7), na.rm=T)
     cols <- grey(2:(length(brks))/length(brks))
     dens <- (2:length(brks))*3
     plot(india,col=cols[findInterval(rrt, brks, all.inside=TRUE)])
-    
+
   })
-  
+
+  # Include at the bottom
+  appid = "appl12"
+  setupStorage(appId = appid, inputs = TRUE)
 }))
