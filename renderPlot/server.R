215080e27a9094577b6ff76b665bdbb6ecbb6835
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 22:34:33 -0500

    Added shinyStorePlus

--- renderPlot/server.R
+++ renderPlot/server.R
@@ -3,31 +3,34 @@ data(iris) # Load the iris dataset
 
 
 shinyServer(
-  
-  
-  function(input, output) {
-    
-    output$text1 <- renderText({ 
+
+
+  function(input, output, session) {
+
+    output$text1 <- renderText({
       colm = as.numeric(input$var)
       paste("Data set variable/column name is", names(iris[colm]))
-      
+
     })
-    
-    output$text2 <- renderText({ 
+
+    output$text2 <- renderText({
       paste("Color of histogram is", input$radio)
     })
-    
-    output$text3 <- renderText({ 
+
+    output$text3 <- renderText({
       paste("Number of histogram BINs is", input$bin)
     })
-    
+
   output$myhist <- renderPlot(
-    
+
     {
     colm = as.numeric(input$var)
     hist(iris[,colm], col =input$colour, xlim = c(0, max(iris[,colm])), main = "Histogram of Iris dataset", breaks = seq(0, max(iris[,colm]),l=input$bin+1), xlab = names(iris[colm]))}
-    
-    )    
+
+    )
+
+  # Include at the bottom
+  appid <- "add63381"
+  setupStorage(appId = appid, inputs = TRUE)
   }
   )
- 
