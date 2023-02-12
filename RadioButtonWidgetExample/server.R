cb52435e0f2a70b13e1c6c91848652e69c72e960
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 21:09:53 -0500

    Added shinyStorePlus

--- RadioButtonWidgetExample/server.R
+++ RadioButtonWidgetExample/server.R
@@ -1,25 +1,25 @@
 library(shiny)
 
 shinyServer(
-  
+
   function(input, output,session) {
-    
+
     output$myname <- renderText({
      # paste("My Name is :", input$name)
       input$name
       })
-    
+
     output$myage <- renderText({
       # paste("My Age is : ", input$age)
       input$age
     })
-    
+
     output$mygender <- renderText({
       #paste("My Age is : ", input$gender)
       input$gender
     })
-   
-    appid = "application501"
+
+    appid = "applicat501"
   setupStorage(appId = appid,inputs = TRUE)
 
   }
