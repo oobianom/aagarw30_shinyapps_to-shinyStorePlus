db983d3c4e4b0159efcf5fd33ed5d1575d3e1e08
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 22:44:44 -0500

    Added shinyStorePlus

--- renderUI Example2/server.R
+++ renderUI Example2/server.R
@@ -1,9 +1,9 @@
 library(shiny)
-shinyServer(function(input,output){
-  
+shinyServer(function(input,output,session){
+
   output$tabs = renderUI({
 
-# usual syntax of tabset - 
+# usual syntax of tabset -
 ## tabsetPanel(tabPanel(title="")) ## for a single tab in the tabsetPanel,
 ## tabsetPanel(tabPanel(title=""), tabPanel(title="")) ## for two tabs in the tabsetPanel..and so on ..
 ## tabsetPanel(tabPanel(title=""), tabPanel(title=""),,......,, tabPanel(title="")) ## for n tabs in the tabsetPanel..and so on ..
@@ -11,16 +11,18 @@ shinyServer(function(input,output){
 #     using lapply(), we will apply the tabPanel function on each of tab title to get a list of tabPanel(s)
     # paste("tab no", 1:input$n, sep="-")
     Tabs <- lapply(paste("tab no.", 1:input$n, sep=" "), tabPanel)
-  
+
 # now we have the list of arguments and we can use the tabsetPanel function on this list of arguments and this could be achieved using the do.call() function
 # do.call function allows you to call any R function, but instead of writing out the arguments one by one, you can use a list to hold the arguments of the function.
-   
+
     do.call(tabsetPanel, Tabs)
 
   })
-  
-  
 
-  
+  # Include at the bottom
+  appid = "appl451f"
+  setupStorage(appId = appid, inputs = TRUE)
+
+
 })
 
