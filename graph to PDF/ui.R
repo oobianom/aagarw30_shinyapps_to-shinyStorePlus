54cfdce6361f9f7c1aa172545554112e85593f74
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 20:51:44 -0500

    Added shinyStorePlus

--- graph to PDF/ui.R
+++ graph to PDF/ui.R
@@ -1,11 +1,14 @@
 library(shiny)
+library(shinyStorePlus)
 shinyUI(fluidPage(
+  initStore(),
   titlePanel("Download PDF with multi plots in Shiny - an example"),
   sidebarLayout(
     sidebarPanel(
+      numericInput("plotnum","Plots multiplier",4),
       downloadButton(outputId = "down", label = "Download the plot")
       ),
     mainPanel()
   )
-  
+
 ))
