a056df1ee4911e412d3c79cb95fb3281d93a0da6
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 21:53:52 -0500

    Added shinyStorePlus

--- SliderExample/server.R
+++ SliderExample/server.R
@@ -1,7 +1,11 @@
 library(shiny)
-shinyServer(function(input, output){
-  
+shinyServer(function(input, output, session){
+
   output$out <- renderText(
     paste("You selected the value: ", input$slide))
-  
+
+  # Include at the bottom
+  appid = "sam67"
+  setupStorage(appId = appid, inputs = TRUE)
+
 })
--- SliderExample/ui.R
+++ SliderExample/ui.R
@@ -1,17 +1,17 @@
-
 library(shiny)
-
+library(shinyStorePlus)
 shinyUI(fluidPage(
+  initStore(),
   titlePanel("Demostration of sliderInput widget in shiny"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("slide", "Select the value from Slider", min = 0, max=5, value=2, step=0.2)
-      
+
       ),
     mainPanel(
-      
+
       textOutput("out")
       )
-    
+
     )
   ))
