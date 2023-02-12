7be98f9f649ddb8763ad9302df783b9b1b26da14
Obi Obianom <oobianom@umaryland.edu>
2023-02-11 22:15:35 -0500

    Added shinyStorePlus

--- reactiveValues Example 1/server.R
+++ reactiveValues Example 1/server.R
@@ -2,21 +2,29 @@ library(shiny)
 shinyServer(
   function(input, output, session) {
     counter <- reactiveValues(countervalue = 0) # Defining & initializing the reactiveValues object
-    
+
     observeEvent(input$add1, {
-      counter$countervalue <- counter$countervalue + 1     # if the add button is clicked, increment the value by 1 and update it
+      counter$countervalue <- counter$countervalue + 1 # if the add button is clicked, increment the value by 1 and update it
+      updateTextInput(session, "tots", value = counter$countervalue)
     })
     observeEvent(input$sub1, {
-      counter$countervalue <- counter$countervalue - 1  # if the sub button is clicked, decrement the value by 1 and update it
+      counter$countervalue <- counter$countervalue - 1 # if the sub button is clicked, decrement the value by 1 and update it
+      updateTextInput(session, "tots", value = counter$countervalue)
     })
     observeEvent(input$reset, {
-      counter$countervalue <- 0                     # if the reset button is clicked, set the counter value to zero
+      updateTextInput(session, "tots", value = 0)
     })
+
+    observeEvent(input$tots, {
+      counter$countervalue <-input$tots
+    })
+
     output$count <- renderText({
-      paste("Counter Value is ", counter$countervalue)   # print the latest value stored in the reactiveValues object
+      paste("Counter Value is ", input$tots) # print the latest value stored in the reactiveValues object
     })
-    
-    
+
+    # Include at the bottom
+    appid <- "add601"
+    setupStorage(appId = appid, inputs = TRUE)
   }
-  
 )
--- reactiveValues Example 1/ui.R
+++ reactiveValues Example 1/ui.R
@@ -1,13 +1,16 @@
 library(shiny)
+library(shinyStorePlus)
 shinyUI(
   fluidPage(
+    initStore(),
     tags$b("Simple counter using reactiveValues() - An example"),
     br(),
     actionButton("add1", "+ 1"),
     actionButton("sub1", "- 1"),
+    numericInput("tots", "Total", 0),
     actionButton("reset", "set to 0"),
     br(),
     textOutput("count")
   )
-  
+
 )
